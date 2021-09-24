7de4a449055a361c511d3a85b893969270ae22e1
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2021-09-24 09:01:30 +0900

    Board投稿機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,27 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
   def index
+  end
+
+  def show
+  end
+
+  def new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :content)
   end
 end
