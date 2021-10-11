3e42ef02278aac86557254cf178ccd51d4998369
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2021-10-11 17:56:26 +0900

    Taskの追加機能を実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def new
+    board = Board.find(params[:board_id])
+    @task = board.tasks.build
+  end
+
+  def create
+    board = Board.find(params[:board_id])
+    @task = board.tasks.build(task_params)
+    @task.user_id = current_user.id
+    if @task.save
+      redirect_to board_task_path(board, @task)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline)
+  end
+end
