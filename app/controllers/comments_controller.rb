72b2d1771d8f008a7480869892f2b6dec5cfb070
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2021-10-25 14:45:41 +0900

    コメント投稿機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def new
+    task = Task.find(params[:task_id])
+    @comment = task.comments.build
+  end
+
+  def create
+    task = Task.find(params[:task_id])
+    @comment = task.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to board_task_path(task.board, task)
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -19,6 +19,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :board
-  has_many :tasks, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :eyecatch
 end
