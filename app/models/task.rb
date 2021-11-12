be599e77a7dcd88c70e0cf9ae89a4cee12191203
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2021-11-12 11:18:42 +0900

    検証とエラーメッセージ

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,6 +17,9 @@
 #  index_tasks_on_user_id   (user_id)
 #
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
   belongs_to :user
   belongs_to :board
   has_many :comments, dependent: :destroy
